063371c81f7e8199bb058f23b9cdf4fec4eb4ef2
Poornendra Parouha <poornendraparouha@gmail.com>
2024-08-13 11:17:56 +0530

    Navigating using Navlink

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, NavLink } from "react-router-dom";
 
 function Navbar (){
     return (
         <>
             <div className="nav">
-            <Link to="/"> <h4>HOME</h4> </Link>
+                <NavLink style={({isActive}) => isActive? {color:'blue'} : undefined } to="/home"><h4>HOME</h4></NavLink>
+                <NavLink style={({isActive}) => isActive? {color:'blue'} : undefined } to="/about"><h4>ABOUT</h4></NavLink>
+                <NavLink style={({isActive}) => isActive? {color:'blue'} : undefined } to="/items"><h4>ITEMS</h4></NavLink>
+            
+            {/* changed all the link tags to navlink  */}
+
+            {/* <Link to="/"> <h4>HOME</h4> </Link>
             <Link to="/about"> <h4>ABOUT</h4> </Link>
-            <Link to="/items"> <h4>ITEMS</h4> </Link>
+            <Link to="/items"> <h4>ITEMS</h4> </Link> */}
 
         </div>
 
