caafae296d689c5a7c3c87d2bb2373d0afdd9362
Poornendra Parouha <poornendraparouha@gmail.com>
2024-08-14 13:03:35 +0530

    added some more on errorpage

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -4,16 +4,20 @@ function ErrorPage (){
     const navigate = useNavigate();
 
     useEffect(()=>{
-        setTimeout(()=> navigate(-1), 3000);
-    }, []);
+        setTimeout(()=> navigate("/"), 20000);
+    }, [navigate]);
 
     return (
         <>
-        
-
             <h1> OOPS!!!!! Something Went Wrong..... </h1>
-
-
+            <div className="not-found page">
+            <h3>Page not found.</h3>
+            <img
+                src="https://cdn-icons-png.flaticon.com/512/2748/2748558.png"
+                alt="not found"
+            />
+            <h3><button onClick={() => navigate(-1)}>Back to previous page...</button></h3>
+            </div>
 
         </>
     )
